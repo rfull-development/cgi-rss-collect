4d7ebfb6604552bb4877f7b91bbc2c3022c0fbc5
Ryota Funaki <ryota.funaki@ngv.jp>
2023-07-05 10:17:10 +0900

    CGI Implement (#2) #2

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # cgi-rss-collect
 This repository is a CGI for collector of RSS.
+
+## Get Started
+### Execute on Linux
+1. Intialize dependencies
+    ```bash
+    go mod tidy
+    ```
+1. Run program
+    ```bash
+    go run main.go
+    ```
--- collect/web.go
+++ collect/web.go
@@ -5,11 +5,13 @@ package collect
 import (
 	"io"
 	"net/http"
+	"time"
 )
 
 // Web is a Raw implementation for downloading raw data from a Web.
 type Web struct {
-	url string
+	url     string
+	timeout time.Duration
 }
 
 // NewWeb creates a new Web instance.
@@ -17,13 +19,28 @@ func NewWeb(url string) *Web {
 	instance := &Web{
 		url: url,
 	}
+	instance.SetTimeout(30)
 	return instance
+}
+
+// SetTimeout sets the timeout for the Web instance.
+func (instance *Web) SetTimeout(value time.Duration) {
+	instance.timeout = value * time.Second
+}
+
+// HttpClient returns a new http.Client instance.
+func (instance *Web) HttpClient() *http.Client {
+	client := &http.Client{
+		Timeout: instance.timeout,
+	}
+	return client
 }
 
 // Download downloads raw data from a URL.
 func (instance *Web) Download() ([]byte, error) {
 	url := instance.url
-	r, e := http.Get(url)
+	client := instance.HttpClient()
+	r, e := client.Get(url)
 	if e != nil {
 		return nil, e
 	}
